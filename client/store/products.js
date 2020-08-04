a866eb7d5669978bcfd3cc3ef4db7fad422114ee
Kevin Moy MAC <kmoy2014@gmail.com>
2020-08-04 15:25:57 -0400

    Added redux skeleton for all products, and added capital letters for components

---
+++ client/store/products.js
@@ -0,0 +1,43 @@
+import axios from 'axios'
+
+/**
+ * Action Types
+ */
+const SET_PRODUCTS = 'SET_PRODUCTS'
+/**
+ * Action Creators
+ */
+export const setProducts = products => ({
+  type: SET_PRODUCTS,
+  products
+})
+
+/**
+ * Thunk Creators
+ */
+
+export function fetchProducts() {
+  return async function(dispatch) {
+    try {
+      // const {data: products} = await axios.get('/api/chair')
+      // dispatch(setProducts(products))
+    } catch (err) {
+      console.error(err)
+    }
+  }
+}
+
+/**
+ * Reducer
+ */
+
+const initialState = []
+
+export default function productsReducer(state = initialState, action) {
+  switch (action.type) {
+    case SET_PRODUCTS:
+      return action.products
+    default:
+      return state
+  }
+}
