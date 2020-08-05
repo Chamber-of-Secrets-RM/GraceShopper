ce3f1e35f8615003e241902c4f8724caf3e5cfd1
Britt Preyer <britt_preyer@britts-mac-mini.lan>
2020-08-05 12:58:32 -0400

    added update quantity Thunk reducer etc.

--- client/store/order.js
+++ client/store/order.js
@@ -5,6 +5,7 @@ import axios from 'axios'
  */
 const SET_ORDER = 'SET_ORDER'
 const REMOVE_ITEM = 'REMOVE_ITEM'
+const UPDATE_QUANTITY = 'UPDATE_QUANTITY'
 /**
  * Action Creators
  */
@@ -17,6 +18,13 @@ export const removeItem = productId => {
   return {
     type: REMOVE_ITEM,
     productId
+  }
+}
+export const updateQuantity = (productId, quantity) => {
+  return {
+    type: UPDATE_QUANTITY,
+    productId,
+    quantity
   }
 }
 /**
@@ -33,11 +41,23 @@ export function fetchOrder(orderId) {
     }
   }
 }
-export function deleteItem(orderId) {
+export function deleteItem(orderId, productId) {
   return async function(dispatch) {
     try {
-      const {data} = await axios.put(`/api/order/${orderId}`)
-      dispatch(removeItem(data.id))
+      await axios.delete(`/api/throughTable/${orderId}/${productId}`)
+      dispatch(removeItem(productId))
+    } catch (err) {
+      console.error(err)
+    }
+  }
+}
+export function changeQuantity(orderId, productId, quantity) {
+  return async function(dispatch) {
+    try {
+      await axios.put(`/api/throughTable/${orderId}/${productId}`, {
+        quantity: quantity
+      })
+      dispatch(updateQuantity(productId, quantity))
     } catch (err) {
       console.error(err)
     }
@@ -56,6 +76,13 @@ export default function orderReducer(state = initialState, action) {
       return action.order
     case REMOVE_ITEM:
       return state.order.filter(product => product.id !== action.productId)
+    case UPDATE_QUANTITY:
+      return state.order.map(product => {
+        if (product.id === action.productId) {
+          product.quantity = action.quantity
+        }
+        return product
+      })
     default:
       return state
   }
