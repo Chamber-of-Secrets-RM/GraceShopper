75105b248c84b9ead5762774b3b463488be92d17
Michael Pesek <pesekm@dupage.edu>
2020-08-09 10:55:00 -0500

    installed emotion core and emotion style, basic navbar styling for demo

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -3,38 +3,83 @@ import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
 import {logout} from '../store'
+import styled from '@emotion/styled'
 
 /**
  * COMPONENT
  */
+const NavBarBody = styled.div`
+  background-color: blue;
+  box-shadow: 5px 10px;
+  border-radius: 4px;
+  display: flex;
+  height: 3rem;
+  align-items: center;
+`
+const LoginDiv = styled.div`
+  width: 100%;
+  border-radius: 4px;
+  display: flex;
+  justify-content: space-between;
+`
+const LoginLinks = styled.div`
+  width: 100%;
+  border-radius: 4px;
+  display: flex;
+  justify-content: flex-end;
+  margin-right: 3rem;
+`
+const PageLinks = styled.div`
+  width: 100%;
+  border-radius: 4px;
+  display: flex;
+  margin-left: 3rem;
+`
 
 const Navbar = ({handleClick, isLoggedIn}) => (
-  <div>
-    <h1>Chamber of Secrets</h1>
-    <nav>
-      {isLoggedIn ? (
-        <div>
-          {/* The navbar will show these links after you log in */}
-          <Link to="/home">Home</Link>
-          <Link to="/shop">Shop</Link>
-          <Link to="/cart">Cart</Link>
-          <a href="#" onClick={handleClick}>
-            Logout
-          </a>
-        </div>
-      ) : (
-        <div>
-          <Link to="/home">Home</Link>
-          <Link to="/shop">Shop</Link>
-          <Link to="/cart">Cart</Link>
-          {/* The navbar will show these links before you log in */}
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Sign Up</Link>
-        </div>
-      )}
-    </nav>
-    <hr />
-  </div>
+  <NavBarBody>
+    {isLoggedIn ? (
+      <PageLinks>
+        {/* The navbar will show these links after you log in */}
+        <Link className="nav-link" to="/home">
+          Home
+        </Link>
+        <Link className="nav-link" to="/shop">
+          Shop
+        </Link>
+        <Link className="nav-link" to="/cart">
+          Cart
+        </Link>
+        <a href="#" onClick={handleClick}>
+          Logout
+        </a>
+      </PageLinks>
+    ) : (
+      <LoginDiv>
+        <PageLinks>
+          <Link className="nav-link" to="/home">
+            Home
+          </Link>
+          <Link className="nav-link" to="/shop">
+            Shop
+          </Link>
+          <Link className="nav-link" to="/cart">
+            Cart
+          </Link>
+        </PageLinks>
+
+        {/* The navbar will show these links before you log in */}
+        <LoginLinks>
+          <Link className="nav-link" to="/login">
+            Login
+          </Link>
+          <Link className="nav-link" to="/signup">
+            Sign Up
+          </Link>
+        </LoginLinks>
+      </LoginDiv>
+    )}
+  </NavBarBody>
 )
 
 /**
