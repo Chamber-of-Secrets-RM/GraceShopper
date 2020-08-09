11484405ef32aed1dfb75a9ea4d65f22c049b440
Kevin Moy MAC <kmoy2014@gmail.com>
2020-08-09 12:31:59 -0400

    fixed google callback route

--- client/components/auth-form.js
+++ client/components/auth-form.js
@@ -30,7 +30,9 @@ const AuthForm = props => {
         </div>
         {error && error.response && <div> {error.response.data} </div>}
       </form>
-      <a href="/auth/google">{displayName} with Google</a>
+      <a target="_self" href="/auth/google">
+        {displayName} with Google
+      </a>
     </div>
   )
 }
